7d59971e898ad03f49931d174243f724592b3c01
AngeloG16 <angelo163017ales@gmail.com>
2023-05-05 23:23:38 -0500

    Veterinaria base de datos relacional

---
+++ datos veterinaria.ts
@@ -0,0 +1,19 @@
+export class Personas{
+    nombre:string;
+    apellido:string;
+    edad:number;
+    cedula:number;
+}
+
+export class Animales{
+    nombre:string;
+    genero:string;
+    raza:string;
+}
+
+export class Veterinaria{
+    nombre:string;
+    Direccion:string;
+    Telefono:number;
+
+}
